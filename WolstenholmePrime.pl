1c500e1800a47f841903cac88444c0465d81284d
biancamares13 <biancamares13@yahoo.com>
2018-05-08 14:36:10 +0300

    working

--- WolstenholmePrime.pl
+++ WolstenholmePrime.pl
@@ -6,13 +6,14 @@ isPrime(2) :- true,!.
 isPrime(X) :- X < 2,!,false.
 isPrime(X) :- not(divisible(X, 2)).
 
-harmonic(N,1,_,_):-harmonic(N,2,1,1).
-harmonic(N,Aux, Denominator, Numerator):- Aux<N,!,
+harmonic(N,1,_,_,Ne,De):-harmonic(N,2,1,1,Ne,De),!.
+harmonic(N,Aux, Denominator, Numerator,De,Ne):- Aux<N,!,
     lcm(Denominator, Aux, LCM),!,
     Numerator1 is Numerator*(LCM/Denominator)+(LCM/Aux),
-    Denominator1 is LCM,!,
-    Aux1 is Aux+1,!,
-    harmonic(N,Aux1, Denominator1, Numerator1).
+    Denominator1 is LCM,
+    Aux1 is Aux+1,
+    harmonic(N,Aux1, Denominator1, Numerator1,De,Ne).
+harmonic(N,N,Denominator,Numerator,Denominator, Numerator):-!.
 
 
 gcd(X, Y, G) :- X = Y, G = X.
@@ -29,13 +30,10 @@ wolstenholme([],[]).
 wolstenholme([H|T],[H|R]):-isPrime(H),
     !,
     Z is H*H*H,
-    harmonic(H,1,_,Numerator),
+    harmonic(H,1,1,1,_,Numerator),
     0 is Numerator mod Z,!,
     wolstenholme(T,R).
 wolstenholme([_|T],R):-wolstenholme(T,R).
-
-
-
 
 
 
